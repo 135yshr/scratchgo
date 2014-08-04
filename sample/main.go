1aa8e10222934df343d852a3887fc99b2b959879
isago <yoshihiro.isago@lasi.co.jp>
2014-08-04 15:55:51 +0900

    move to sample directory.

---
+++ sample/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	_ "io/ioutil"
+	"os"
+	"scratchgo/scratch"
+	"strings"
+)
+
+func main() {
+	conn, err := scratch.NewDefaultConnect()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	go func() {
+		for {
+			msg, err := conn.Recv()
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println(*msg)
+		}
+	}()
+
+	for {
+		fmt.Printf("> ")
+		reader := bufio.NewReader(os.Stdin)
+		line, err := reader.ReadString('\n')
+		exitOnErr("read buffer", err)
+
+		line = strings.Replace(line, "\n", "", -1)
+		buff := strings.Split(line, " ")
+
+		switch strings.ToLower(buff[0]) {
+		case "send":
+			err = conn.SensorUpdate(buff[1], buff[2:])
+			exitOnErr("update sensor", err)
+		case "broadcast":
+			err = conn.BroadCast(buff[1:])
+			exitOnErr("broadcast message", err)
+		case "exit", "quit":
+			os.Exit(0)
+		default:
+			fmt.Println("unknown command.\n  send or broadcast")
+		}
+	}
+	// for {
+	// 	data := make([]byte, 255)
+	// 	_, err := conn.Read(data)
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	}
+	// 	fmt.Println(data)
+	// }
+	// data, err := ioutil.ReadAll(conn)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println(data)
+	os.Exit(0)
+}
+
+func exitOnErr(msg string, err error) {
+	if err != nil {
+		fmt.Println(msg, err)
+		os.Exit(1)
+	}
+}
